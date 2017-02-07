c339b6b6c7c010e90b6a3f238badd7d5085d50d9
Claudio Fernandez <claudiof@gmail.com>
2017-02-07 15:19:20 -0300

    Agregado el modulo body-parser
    Agregado el modulo body-parser y configurado express
    para hacer uso del middleware que parsea el JSON
    de cada pedido HTTP.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 
+app.use(bodyParser.json())
+
+// This is temporal!
 let noticias = []
 
 app.get('/noticias', (req, res) => {
-  res.status(200).send(noticias)
+  res.json(noticias)
 })
 
 app.post('/noticias', (req, res) => {
   let noticia = req.body
-
   noticias.push(noticia)
-  res.status(200).send(noticia)
+
+  res.sendStatus(200)
 })
 
 const port = 3001
