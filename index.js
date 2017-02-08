320b908115083b7325bdf073f5f46f43c2b7b69b
Claudio Fernandez <claudiof@gmail.com>
2017-02-08 14:27:14 -0300

    Agregado mongoose + mongodb, noticias persistidas

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost/news')
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  upvotes: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+})
+
+const Post = mongoose.model('Post', postSchema)
+
 const app = express()
 
 app.use(bodyParser.json())
 
-// This is temporal!
-let noticias = []
 
-app.get('/noticias', (req, res) => {
-  res.json(noticias)
+app.get('/noticias', (req, res, next) => {
+  Post.find((err, noticias) => {
+    if (err) {
+      return next(err)
+    }
+
+    res.json(noticias)
+  })
 })
 
-app.post('/noticias', (req, res) => {
-  let noticia = req.body
-  noticias.push(noticia)
+app.post('/noticias', (req, res, next) => {
+  const noticia = new Post(req.body)
 
-  res.sendStatus(200)
+  noticia.save((err, noticia) => {
+    if (err) {
+      return next(err)
+    }
+
+    res.sendStatus(200)
+  })
 })
 
 const port = 3001
