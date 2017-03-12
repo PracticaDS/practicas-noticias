101c89778c5370082644cba3e6877e4d3334e06b
Claudio Fernandez <claudiof@gmail.com>
2017-03-12 14:47:24 -0300

    Added comments

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import Post from '../models/Post.js'
+import Comment from '../models/Comment.js'
 
 let router = express.Router()
 
@@ -33,18 +34,34 @@ router.post('/noticias', (req, res, next) => {
     .catch(next)
 })
 
-router.get('/noticias/:noticia', (req, res) => {
-  res.json(req.noticia)
+router.get('/noticias/:noticia', (req, res, next) => {
+  req.noticia.populate('comments').execPopulate()
+    .then(noticiaCompleta => res.json(noticiaCompleta))
+    .catch(next)
 })
 
 router.put('/noticias/:noticia/upvote', (req, res, next) => {
-  const noticia = req.noticia;
-  noticia.upvote();
+  const noticia = req.noticia
+  noticia.upvote()
 
   noticia.save()
     .then(noticiaGuardada => res.json(noticiaGuardada))
     .catch(next)
 })
 
+router.post('/noticias/:noticia/comentarios', (req, res, next) => {
+  const noticia = req.noticia
+  let comentario = new Comment(req.body)
+  comentario.post = noticia
+
+  comentario.save()
+    .then(comentarioGuardado => {
+      comentario = comentarioGuardado
+      noticia.comments.push(comentario)
+      return noticia.save()
+    })
+    .then(noticiaGuardada => res.json(comentario))
+    .catch(next)
+})
 
 export default router
