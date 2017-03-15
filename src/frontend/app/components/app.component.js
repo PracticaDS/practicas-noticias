92014b97125dd9f79cba73893be80619c9512a49
Claudio Fernandez <claudiof@gmail.com>
2017-03-15 00:56:40 -0300

    NewPostComponent

--- src/frontend/app/components/app.component.js
+++ src/frontend/app/components/app.component.js
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 
+import PostService from '../services/post.service';
+
 @Component({
   selector: 'app-view',
   template: `<h1>Bienvenidos a {{name}}</h1>
             <post *ngFor="let item of posts" [data]="item"></post>
-            `
+
+            <newPost></newPost>`,
+  providers: [ PostService ]
 })
 export default class AppComponent {
-  constructor() {
+  constructor(postService) {
     this.name = 'Noticias UNQ',
-    this.posts = [
-      { title: 'Noticia 1', content: 'Contenido 1', author: 'Claudio', upvotes: 1 },
-      { title: 'Noticia 2', content: 'Contenido 2', author: 'Claudio', upvotes: 3 },
-      { title: 'Noticia 3', content: 'Contenido 3', author: 'Claudio', upvotes: 0 }
-    ]
+    this.posts = postService.posts;
   }
 }
+
+AppComponent.parameters = [
+  [PostService]
+]
--- src/frontend/bootstrap.js
+++ src/frontend/bootstrap.js
@@ -4,16 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
 
 import AppComponent from './app/components/app.component'
 import PostComponent from './app/components/post.component'
-
+import NewPostComponent from './app/components/newPost.component'
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FormsModule ],
   styleUrls: ['./style.css'],
   declarations: [
     PostComponent,
+    NewPostComponent,
     AppComponent
   ],
   bootstrap: [ AppComponent ]
