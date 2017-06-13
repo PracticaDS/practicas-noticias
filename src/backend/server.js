e576deb3a132f5612110044ac84b6bd064132f8a
Javier Fernandes <javier.fernandes@gmail.com>
2017-06-13 17:40:38 -0300

    changing app mongo env var to be able to run in heroku with mongolabs

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,8 @@
 import app from "./app"
 
 import mongoose from 'mongoose'
-const MONGO_SERVER = process.env.MONGO_SERVER || 'localhost'
-mongoose.connect(`mongodb://${MONGO_SERVER}/news`)
+const MONGO_SERVER = process.env.MONGO_SERVER || `mongodb://localhost/news`
+mongoose.connect(MONGO_SERVER)
 
 // Express startup
 const port = process.env.PORT || 3001
